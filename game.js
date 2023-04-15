26bd428e1745f24700a1cdec6d474d2782f0c802
PRAFUL BANDRE <109658545+prafulbbandre@users.noreply.github.com>
2023-04-15 15:02:23 +0530

    the end

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
-
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
@@ -52,6 +53,8 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
